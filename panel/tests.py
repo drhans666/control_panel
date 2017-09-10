8ef62abd37c3a5f3cf8ba637ae85a27e161db20c
drhans666 <k.krasowski87@gmail.com>
2017-09-10 19:52:43 +0200

    -lacking tests for panel added

--- panel/tests.py
+++ panel/tests.py
@@ -3,7 +3,7 @@ from datetime import date, timedelta
 from django.contrib.auth.models import User
 from django.test import TestCase
 
-from .scripts import count_vac_days, vacnow_search_append
+from .scripts import count_vac_days, vacnow_search_append, vac_search
 from .models import Vacation
 
 
@@ -68,17 +68,61 @@ class OnVacationTest(TestCase):
         user1 = User.objects.get(username='tester')
         user2 = User.objects.get(username='tester2')
         start_date = (date.today() - timedelta(days=10))
-        print(start_date)
         end_date = date.today() + timedelta(days=10)
 
         Vacation.objects.create(accepted=True, user=user1,
-                                start_date= date.today(),
+                                start_date=date.today(),
                                 end_date=end_date)
         Vacation.objects.create(accepted=True, user=user2,
-                                start_date= start_date,
+                                start_date=start_date,
                                 end_date=date.today())
 
         found, days_list = vacnow_search_append()
         self.assertEqual(len(found), 2)
         self.assertEqual(days_list[0], '1 day')
         self.assertEqual(days_list[1], '11 days')
+
+
+class VacationSearchTest(TestCase):
+
+    def setUp(self):
+        user1 = User.objects.create(username='tester')
+        user2 = User.objects.create(username='tester2')
+        Vacation.objects.create(accepted=True, user=user1,
+                                add_date=date.today() + timedelta(days=3),
+                                start_date=date.today() - timedelta(days=5),
+                                end_date=date.today() - timedelta(days=3))
+        Vacation.objects.create(accepted=False, user=user2,
+                                start_date=date.today(),
+                                end_date=date.today())
+
+    def test_date_search(self):
+        autodate_override = date.today() - timedelta(days=7)
+        Vacation.objects.filter(user__username='tester').update(add_date=autodate_override)
+        accepted = 'All'
+        query_user = None
+        search_from = date.today() - timedelta(days=5)
+        search_to = str(date.today()) + ' 23:59:59'
+        found_list = vac_search(accepted, search_from, search_to, query_user)
+
+        self.assertEqual(len(found_list), 1)
+
+    def test_accept_search(self):
+        accepted = True
+        query_user = None
+        search_from = date.today()
+        search_to = str(date.today()) + ' 23:59:59'
+        found_list = vac_search(accepted, search_from, search_to, query_user)
+
+        self.assertEqual(len(found_list), 1)
+        self.assertEqual(found_list[0].user.username, 'tester')
+
+    def test_user_search(self):
+        accepted = 'All'
+        query_user = User.objects.filter(username='tester2')
+        search_from = date.today()
+        search_to = str(date.today()) + ' 23:59:59'
+        found_list = vac_search(accepted, search_from, search_to, query_user)
+
+        self.assertEqual(len(found_list), 1)
+        self.assertEqual(found_list[0].user.username, 'tester2')
